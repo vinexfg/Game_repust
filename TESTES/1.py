efdba19dcbdee3cb3625ba1de1e132eaa9ae66f7
Vinicius Almeida <vinysilvaa20@gmail.com>
2025-01-26 20:08:02 -0300

    Update 1.py

--- TESTES/1.py
+++ TESTES/1.py
@@ -1,1 +1,43 @@
 print('TESTES')
+
+
+class Personagem:
+    def __init__(self, nome, sexo) -> None:
+        self.nome = nome
+        self.sexo = sexo
+        self.dano = 5
+        self.lv = 1
+        self.xp = 5
+    
+    #criar um arquivo de erro. 
+
+    def personagem_base(self):
+        self.nome = input('Nome: ')
+        self.sexo = input('Sexo M/F : ')
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+# class classe_do_personagem:
+
+#     def guerreiro(self):
+#         dano = 15
+#         defesa = 10
+#         vidas = 120
+
+
+        
+        
+
