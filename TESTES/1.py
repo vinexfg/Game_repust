82ca1a686c1de64048e076340aae2c55b61f5c1d
Rodrygo88 <2rodrigosilva2@gmail.com>
2025-01-30 10:35:40 -0300

    menu novo jogo
    criação de personagem

--- TESTES/1.py
+++ TESTES/1.py
@@ -1,32 +1,134 @@
 import tkinter as tk
 
+def frame1():
+    for widget in janela.winfo_children():
+        widget.destroy()
+
+    frame_menu = tk.Frame(janela, bg="#A67C58")
+    frame_menu.pack(fill="both", expand=True)
+
+    ##----------------------------------------------------------------------------------------
+    ## FRAME MENU
+
+    label_logo = tk.Label(frame_menu, text="REPUST", fg="#4EA68D", bg="#A67C58", font=("Agency FB", 100, "bold"))
+    label_logo.pack()
+
+    label = tk.Label(frame_menu, text="RPG", fg="#F2994B", bg="#A67C58", font=("Agency FB", 20, "bold"))
+    label.pack()
+
+    ##------
+
+    botao_novo = tk.Button(frame_menu, text="NOVO JOGO", bg="#4EA68D", font=("Agency FB", 20), fg="#F2F2F2", bd=5, relief="ridge", command=frame2)
+    botao_novo.pack(pady=10)
+
+    botao_carregar = tk.Button(frame_menu, text="CARREGAR JOGO", bg="#4EA68D", font=("Agency FB", 20), fg="#F2F2F2", bd=5, relief="ridge")
+    botao_carregar.pack(pady=10)
+
+    botao_conf = tk.Button(frame_menu, text="CONFIGURAÇÕES", bg="#4EA68D", font=("Agency FB", 20), fg="#F2F2F2", bd=5, relief="ridge")
+    botao_conf.pack(pady=10)
+
+    botao_sair = tk.Button(frame_menu, text="SAIR", bg="#4EA68D", font=("Agency FB", 20), fg="#F2F2F2", bd=5, relief="ridge")
+    botao_sair.pack(pady=10)
+
+    label_creditos = tk.Label(frame_menu, text="@rodrigo_silva @vinicius_almeida", fg="#F2F2F2", bg="#A67C58", font=("Agency FB", 10, "bold"))
+    label_creditos.pack(side="bottom")
+
+    ##----------------------------------------------------------------------------------------
+
+def frame2():
+    for widget in janela.winfo_children():
+        widget.destroy()
+
+    frame_novo = tk.Frame(janela, bg="#A67C58")
+    frame_novo.pack(fill="both", expand=True)
+
+
+    ##----------------------------------------------------------------------------------------
+    ## FRAME NOVO
+
+    label_logo = tk.Label(frame_novo, text="NOVO JOGO", fg="#4EA68D", bg="#A67C58", font=("Agency FB", 75, "bold"))
+    label_logo.pack()
+
+    label = tk.Label(frame_novo, text="Criação de personagem", fg="#F2994B", bg="#A67C58", font=("Agency FB", 25, "bold"))
+    label.pack()
+
+
+    label_nome = tk.Label(frame_novo, text="NOME DO PERSONAGEM", fg="#F2F2F2", bg="#A67C58", font=("Agency FB", 15, "bold"))
+    label_nome.pack()
+
+    caixa_nome = tk.Text(frame_novo, fg="#4EA68D", bg="#F2F2F2", font=("Agency FB", 15, "bold"),  width=40, height=0)
+    caixa_nome.pack()
+
+    grupo_classes = tk.Frame(frame_novo, bg="#A67C58")
+    grupo_classes.pack()
+
+    grupo_raca = tk.Frame(frame_novo, bg="#A67C58")
+    grupo_raca.pack()
+
+    ##---
+
+    label_classe = tk.Label(grupo_classes, text="CLASSE DO PERSONAGEM", fg="#F2F2F2", bg="#A67C58", font=("Agency FB", 15, "bold"))
+    label_classe.pack()
+    
+    classe1 = tk.Button(grupo_classes, text="GUERREIRO", width=20, height=0, fg="#4EA68D", bg="#F2F2F2", font=("Agency FB", 15, "bold"), bd=5, relief="ridge")
+    classe1.pack( pady=2, padx=5, side="left")
+
+    classe2 = tk.Button(grupo_classes, text="MAGO", width=20, height=0, fg="#4EA68D", bg="#F2F2F2", font=("Agency FB", 15, "bold"), bd=5, relief="ridge")
+    classe2.pack(pady=2, padx=5, side="left")
+
+    classe3 = tk.Button(grupo_classes, text="LADINO", width=20, height=0, fg="#4EA68D", bg="#F2F2F2", font=("Agency FB", 15, "bold"), bd=5, relief="ridge")
+    classe3.pack(pady=2, padx=5, side="left")
+
+    classe4 = tk.Button(grupo_classes, text="BÁRBARO", width=20, height=0, fg="#4EA68D", bg="#F2F2F2", font=("Agency FB", 15, "bold"), bd=5, relief="ridge")
+    classe4.pack(pady=2, padx=5, side="left")
+
+    ##---
+
+    label_raca = tk.Label(grupo_raca, text="RAÇA DO PERSONAGEM", fg="#F2F2F2", bg="#A67C58", font=("Agency FB", 15, "bold"))
+    label_raca.pack()
+
+    raca1 = tk.Button(grupo_raca, text="HUMANO", width=20, height=0, fg="#4EA68D", bg="#F2F2F2", font=("Agency FB", 15, "bold"), bd=5, relief="ridge")
+    raca1.pack( pady=2, padx=5, side="left")
+
+    raca2 = tk.Button(grupo_raca, text="ELFO", width=20, height=0, fg="#4EA68D", bg="#F2F2F2", font=("Agency FB", 15, "bold"), bd=5, relief="ridge")
+    raca2.pack(pady=2, padx=5, side="left")
+
+    raca3 = tk.Button(grupo_raca, text="ANÃO", width=20, height=0, fg="#4EA68D", bg="#F2F2F2", font=("Agency FB", 15, "bold"), bd=5, relief="ridge")
+    raca3.pack(pady=2, padx=5, side="left")
+
+    raca4 = tk.Button(grupo_raca, text="ORCS", width=20, height=0, fg="#4EA68D", bg="#F2F2F2", font=("Agency FB", 15, "bold"), bd=5, relief="ridge")
+    raca4.pack(pady=2, padx=5, side="left")
+
+    ##---
+    
+    label_sexo = tk.Label(frame_novo, text="SEXO DO PERSONAGEM", fg="#F2F2F2", bg="#A67C58", font=("Agency FB", 15, "bold"))
+    label_sexo.pack()
+
+    sexo1 = tk.Button(frame_novo, text="MASCULINO", width=20, height=0, fg="#4EA68D", bg="#F2F2F2", font=("Agency FB", 15, "bold"), bd=5, relief="ridge")
+    sexo1.pack(pady=2, padx=5)
+
+    sexo2 = tk.Button(frame_novo, text="FEMININO", width=20, height=0, fg="#4EA68D", bg="#F2F2F2", font=("Agency FB", 15, "bold"), bd=5, relief="ridge")
+    sexo2.pack(pady=2, padx=5)
+
+
+
+
+
+
+
+
+    botao_voltar = tk.Button(frame_novo, text="Voltar", bg="#4EA68D", font=("Agency FB", 15), fg="white", bd=5, relief="ridge", command=frame1)
+    botao_voltar.pack(pady=10, side="bottom")
+
+
+    ##----------------------------------------------------------------------------------------
+
 janela = tk.Tk()
 
 janela.title("REPUST RPG")
 janela.geometry("1000x900")
 janela.configure(bg="#A67C58", pady=100)
 
-label_logo = tk.Label(janela, text="REPUST", fg="#4EA68D", bg="#A67C58", font=("Agency FB", 100, "bold"))
-label_logo.pack()
-
-label = tk.Label(janela, text="RPG", fg="#F2994B", bg="#A67C58", font=("Agency FB", 20, "bold"))
-label.pack()
-
-## ------
-
-botao_new = tk.Button(janela, text="NOVO JOGO", bg="#4EA68D", font=("Agency FB", 20), fg="white", bd=5, relief="ridge")
-botao_new.pack(pady=10)
-
-botao_new = tk.Button(janela, text="CARREGAR JOGO", bg="#4EA68D", font=("Agency FB", 20), fg="white", bd=5, relief="ridge")
-botao_new.pack(pady=10)
-
-botao_new = tk.Button(janela, text="CONFIGURAÇÕES", bg="#4EA68D", font=("Agency FB", 20), fg="white", bd=5, relief="ridge")
-botao_new.pack(pady=10)
-
-botao_new = tk.Button(janela, text="SAIR", bg="#4EA68D", font=("Agency FB", 20), fg="white", bd=5, relief="ridge")
-botao_new.pack(pady=10)
-
-label_creditos = tk.Label(janela, text="@rodrigo_silva @vinicius_almeida", fg="white", bg="#A67C58", font=("Agency FB", 10, "bold"))
-label_creditos.pack(side="bottom")
+frame1()
 
 janela.mainloop()
