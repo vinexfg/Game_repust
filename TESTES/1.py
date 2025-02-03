95f4ed27d1c72107c215d30a60ea7163c83cd09b
Vinicius Almeida <vinysilvaa20@gmail.com>
2025-02-03 18:03:09 -0300

    add armazem

--- TESTES/1.py
+++ TESTES/1.py
@@ -6,14 +6,31 @@ class Personagem:
         self.nome = nome
         self.sexo = sexo
         self.dano = 5
+        self.defesa = 2.5
         self.lv = 1
         self.xp = 5
+        self.vida = 100
+        self.classe = ['fogo', 'agua', 'terra', 'ar']
     
     #criar um arquivo de erro. 
 
     def personagem_base(self):
         self.nome = input('Nome: ')
         self.sexo = input('Sexo M/F : ')
+    
+    def armazem_dados(self):
+        Personagem_dict  = {
+            "nome": self.nome,
+            "sexo": self.sexo,
+            "saude": self.defesa,
+            "classe": self.classe,
+            "ataque": self.dano,
+            "defesa": self.defesa,
+            "nivel": self.lv
+
+        }
+        return Personagem_dict
+    
         
 
 
