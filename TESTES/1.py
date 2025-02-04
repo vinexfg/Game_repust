bc1b81cb60bcb7150fb6a412dbf01e552757b9fd
Rodrygo88 <2rodrigosilva2@gmail.com>
2025-02-04 10:52:59 -0300

    interatividade de criação
    personagem

--- TESTES/1.py
+++ TESTES/1.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 
+
+
 def frame1():
     for widget in janela.winfo_children():
         widget.destroy()
@@ -42,6 +44,89 @@ def frame2():
     frame_novo = tk.Frame(janela, bg="#A67C58")
     frame_novo.pack(fill="both", expand=True)
 
+    ##--------------------------------------------------------
+
+    personagem = {"nome": "indefinido", "classe": "indefinido", "raca": "indefinido" ,"sexo": "indefinido"}
+
+    ## --- Classe
+
+    def pegarGuerreiro():
+        personagem["classe"] = "guerreiro"
+        classe1.config(bg="#F2994B", fg="#F2F2F2")
+        classe2.config(fg="#4EA68D", bg="#F2F2F2")
+        classe3.config(fg="#4EA68D", bg="#F2F2F2")
+        classe4.config(fg="#4EA68D", bg="#F2F2F2")
+
+    def pegarMago():
+        personagem["classe"] = "mago"
+        classe2.config(bg="#F2994B", fg="#F2F2F2")
+        classe1.config(fg="#4EA68D", bg="#F2F2F2")
+        classe3.config(fg="#4EA68D", bg="#F2F2F2")
+        classe4.config(fg="#4EA68D", bg="#F2F2F2")
+        
+    def pegarLadino():
+        personagem["classe"] = "ladino"
+        classe3.config(bg="#F2994B", fg="#F2F2F2")
+        classe2.config(fg="#4EA68D", bg="#F2F2F2")
+        classe1.config(fg="#4EA68D", bg="#F2F2F2")
+        classe4.config(fg="#4EA68D", bg="#F2F2F2")
+       
+    def pegarBarbaro():
+        personagem["classe"] = "barbaro"
+        classe4.config(bg="#F2994B", fg="#F2F2F2")
+        classe2.config(fg="#4EA68D", bg="#F2F2F2")
+        classe3.config(fg="#4EA68D", bg="#F2F2F2")
+        classe1.config(fg="#4EA68D", bg="#F2F2F2")
+        
+    ## --- Raça
+
+    def pegarHumano():
+        personagem["raca"] = "humano"
+        raca1.config(bg="#F2994B", fg="#F2F2F2")
+        raca2.config(fg="#4EA68D", bg="#F2F2F2")
+        raca3.config(fg="#4EA68D", bg="#F2F2F2")
+        raca4.config(fg="#4EA68D", bg="#F2F2F2")
+
+    def pegarElfo():
+        personagem["raca"] = "elfo"
+        raca2.config(bg="#F2994B", fg="#F2F2F2")
+        raca1.config(fg="#4EA68D", bg="#F2F2F2")
+        raca3.config(fg="#4EA68D", bg="#F2F2F2")
+        raca4.config(fg="#4EA68D", bg="#F2F2F2")
+        
+    def pegarAnao():
+        personagem["raca"] = "anao"
+        raca3.config(bg="#F2994B", fg="#F2F2F2")
+        raca2.config(fg="#4EA68D", bg="#F2F2F2")
+        raca1.config(fg="#4EA68D", bg="#F2F2F2")
+        raca4.config(fg="#4EA68D", bg="#F2F2F2")
+       
+    def pegarOrc():
+        personagem["raca"] = "orc"
+        raca4.config(bg="#F2994B", fg="#F2F2F2")
+        raca2.config(fg="#4EA68D", bg="#F2F2F2")
+        raca3.config(fg="#4EA68D", bg="#F2F2F2")
+        raca1.config(fg="#4EA68D", bg="#F2F2F2")
+
+    ## -- Sexo
+
+    def pegarMasculino():
+        personagem["sexo"] = "masculino"
+        sexo1.config(bg="#F2994B", fg="#F2F2F2")
+        sexo2.config(fg="#4EA68D", bg="#F2F2F2")
+        
+
+    def pegarFeminino():
+        personagem["sexo"] = "feminino"
+        sexo2.config(bg="#F2994B", fg="#F2F2F2")
+        sexo1.config(fg="#4EA68D", bg="#F2F2F2")
+           
+
+    
+
+    
+    ##--------------------------------------------------------
+
 
     ##----------------------------------------------------------------------------------------
     ## FRAME NOVO
@@ -70,16 +155,16 @@ def frame2():
     label_classe = tk.Label(grupo_classes, text="CLASSE DO PERSONAGEM", fg="#F2F2F2", bg="#A67C58", font=("Agency FB", 15, "bold"))
     label_classe.pack()
     
-    classe1 = tk.Button(grupo_classes, text="GUERREIRO", width=20, height=0, fg="#4EA68D", bg="#F2F2F2", font=("Agency FB", 15, "bold"), bd=5, relief="ridge")
+    classe1 = tk.Button(grupo_classes, text="GUERREIRO", width=20, height=0, fg="#4EA68D", bg="#F2F2F2", font=("Agency FB", 15, "bold"), bd=5, relief="ridge", command=pegarGuerreiro)
     classe1.pack( pady=2, padx=5, side="left")
 
-    classe2 = tk.Button(grupo_classes, text="MAGO", width=20, height=0, fg="#4EA68D", bg="#F2F2F2", font=("Agency FB", 15, "bold"), bd=5, relief="ridge")
+    classe2 = tk.Button(grupo_classes, text="MAGO", width=20, height=0, fg="#4EA68D", bg="#F2F2F2", font=("Agency FB", 15, "bold"), bd=5, relief="ridge", command=pegarMago)
     classe2.pack(pady=2, padx=5, side="left")
 
-    classe3 = tk.Button(grupo_classes, text="LADINO", width=20, height=0, fg="#4EA68D", bg="#F2F2F2", font=("Agency FB", 15, "bold"), bd=5, relief="ridge")
+    classe3 = tk.Button(grupo_classes, text="LADINO", width=20, height=0, fg="#4EA68D", bg="#F2F2F2", font=("Agency FB", 15, "bold"), bd=5, relief="ridge", command=pegarLadino)
     classe3.pack(pady=2, padx=5, side="left")
 
-    classe4 = tk.Button(grupo_classes, text="BÁRBARO", width=20, height=0, fg="#4EA68D", bg="#F2F2F2", font=("Agency FB", 15, "bold"), bd=5, relief="ridge")
+    classe4 = tk.Button(grupo_classes, text="BÁRBARO", width=20, height=0, fg="#4EA68D", bg="#F2F2F2", font=("Agency FB", 15, "bold"), bd=5, relief="ridge", command=pegarBarbaro)
     classe4.pack(pady=2, padx=5, side="left")
 
     ##---
@@ -87,16 +172,16 @@ def frame2():
     label_raca = tk.Label(grupo_raca, text="RAÇA DO PERSONAGEM", fg="#F2F2F2", bg="#A67C58", font=("Agency FB", 15, "bold"))
     label_raca.pack()
 
-    raca1 = tk.Button(grupo_raca, text="HUMANO", width=20, height=0, fg="#4EA68D", bg="#F2F2F2", font=("Agency FB", 15, "bold"), bd=5, relief="ridge")
+    raca1 = tk.Button(grupo_raca, text="HUMANO", width=20, height=0, fg="#4EA68D", bg="#F2F2F2", font=("Agency FB", 15, "bold"), bd=5, relief="ridge", command=pegarHumano)
     raca1.pack( pady=2, padx=5, side="left")
 
-    raca2 = tk.Button(grupo_raca, text="ELFO", width=20, height=0, fg="#4EA68D", bg="#F2F2F2", font=("Agency FB", 15, "bold"), bd=5, relief="ridge")
+    raca2 = tk.Button(grupo_raca, text="ELFO", width=20, height=0, fg="#4EA68D", bg="#F2F2F2", font=("Agency FB", 15, "bold"), bd=5, relief="ridge", command=pegarElfo)
     raca2.pack(pady=2, padx=5, side="left")
 
-    raca3 = tk.Button(grupo_raca, text="ANÃO", width=20, height=0, fg="#4EA68D", bg="#F2F2F2", font=("Agency FB", 15, "bold"), bd=5, relief="ridge")
+    raca3 = tk.Button(grupo_raca, text="ANÃO", width=20, height=0, fg="#4EA68D", bg="#F2F2F2", font=("Agency FB", 15, "bold"), bd=5, relief="ridge", command=pegarAnao)
     raca3.pack(pady=2, padx=5, side="left")
 
-    raca4 = tk.Button(grupo_raca, text="ORCS", width=20, height=0, fg="#4EA68D", bg="#F2F2F2", font=("Agency FB", 15, "bold"), bd=5, relief="ridge")
+    raca4 = tk.Button(grupo_raca, text="ORC", width=20, height=0, fg="#4EA68D", bg="#F2F2F2", font=("Agency FB", 15, "bold"), bd=5, relief="ridge", command=pegarOrc)
     raca4.pack(pady=2, padx=5, side="left")
 
     ##---
@@ -104,17 +189,14 @@ def frame2():
     label_sexo = tk.Label(frame_novo, text="SEXO DO PERSONAGEM", fg="#F2F2F2", bg="#A67C58", font=("Agency FB", 15, "bold"))
     label_sexo.pack()
 
-    sexo1 = tk.Button(frame_novo, text="MASCULINO", width=20, height=0, fg="#4EA68D", bg="#F2F2F2", font=("Agency FB", 15, "bold"), bd=5, relief="ridge")
+    sexo1 = tk.Button(frame_novo, text="MASCULINO", width=20, height=0, fg="#4EA68D", bg="#F2F2F2", font=("Agency FB", 15, "bold"), bd=5, relief="ridge", command=pegarMasculino)
     sexo1.pack(pady=2, padx=5)
 
-    sexo2 = tk.Button(frame_novo, text="FEMININO", width=20, height=0, fg="#4EA68D", bg="#F2F2F2", font=("Agency FB", 15, "bold"), bd=5, relief="ridge")
+    sexo2 = tk.Button(frame_novo, text="FEMININO", width=20, height=0, fg="#4EA68D", bg="#F2F2F2", font=("Agency FB", 15, "bold"), bd=5, relief="ridge", command=pegarFeminino)
     sexo2.pack(pady=2, padx=5)
 
-
-
-
-
-
+    botao_criar = tk.Button(frame_novo, text="Criar Personagem", bg="#4EA68D", font=("Agency FB", 15), fg="white", bd=5, relief="ridge")
+    botao_criar.pack(pady=10)
 
 
     botao_voltar = tk.Button(frame_novo, text="Voltar", bg="#4EA68D", font=("Agency FB", 15), fg="white", bd=5, relief="ridge", command=frame1)
@@ -128,6 +210,7 @@ janela = tk.Tk()
 janela.title("REPUST RPG")
 janela.geometry("1000x900")
 janela.configure(bg="#A67C58", pady=100)
+
 
 frame1()
 
